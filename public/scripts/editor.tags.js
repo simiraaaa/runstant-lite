ebd19a8177488198983c0ab666e2b3f2e2cf5db4
phi <phi1618jp@gmail.com>
2015-07-29 09:21:27 +0900

    fix save and load

--- public/scripts/editor.tags.js
+++ public/scripts/editor.tags.js
@@ -1,6 +1,5 @@
 
 riot.tag('app', '<header></header> <div class="main"> <editor width="60%" height="100%" float="right" onsave="{onsave}" class="panel"></editor> <preview width="40%" height="60%" float="left" class="panel"></preview> <console width="40%" height="40%" float="left" onpost="{onpost}" class="panel"></console> </div> <footer></footer> <detailmodal></detailmodal>', 'body { background: hsl(0, 0%, 95%); } .main { position: absolute; width: 100%; height: calc(100% - 64px - 30px); overflow: hidden; } .panel { display: block; padding: 5px 5px; float: right; transition: 500ms; } .panel.fullscreen { width: 100% !important; height: 100% !important; } .panel.nofullscreen { width: 0% !important; height: 0% !important; opacity: 0.0; margin: 0px; padding: 0px; } .inner { /* border: 1px solid #ccc; */ position: relative; width: 100%; height: 100%; }', function(opts) {
-    runstant.data = JSON.parse( JSON.stringify(runstant.constant.TEMPLATE_DATA) );
     var self = this;
     
     runstant.openDetailModal = function() {
@@ -14,10 +13,49 @@ riot.tag('app', '<header></header> <div class="main"> <editor width="60%" height
     
     this.onsave = function() {
       self.tags.preview.refresh();
+      self.save();
     };
     
     this.onpost = function(v) {
       self.tags.preview.post(v);
+    };
+    
+    var cache = '';
+    this.load = function() {
+      var data = null;
+    
+      if (location.hash) {
+        var hash = location.hash.substr(1);
+        data = runstant.util.hash2json(hash);
+      }
+      else {
+        data = JSON.parse( JSON.stringify(runstant.constant.TEMPLATE_DATA) );
+      }
+
+      if (data.setting.detail && !data.setting.description) {
+        data.setting.description = data.setting.detail;
+      }
+    
+      runstant.data = data;
+
+      cache = JSON.stringify(data);
+
+      document.title = data.setting.title + " | runstant";
+    };
+    this.load();
+    
+    this.save = function() {
+      var data = runstant.data;
+      var dataString = JSON.stringify(data);
+    
+      if (this.cache !== dataString) {
+        this.cache = dataString;
+    
+        var hash = runstant.util.json2hash(data);
+        history.pushState(null, 'runstant', '#' + hash);
+
+        document.title = data.setting.title + " | runstant";
+      }
     };
     
     
@@ -135,7 +173,7 @@ riot.tag('console', '<div class="inner z-depth-2"> <div class="header cyan light
 });
 
 <!-- クリップの詳細-->
-riot.tag('detailmodal', '<div class="modal-content"> <h4>Setting</h4> <form name="_form" class="row"> <div class="col s6"> <h5>Project</h5> <div class="row"> <div class="col s12 input-field"> <input name="_title" value="hoge" type="text"> <label>Project Title</label> </div> <div class="col s12 input-field"> <textarea name="_description" class="materialize-textarea"></textarea> <label>Description</label> </div> </div> <div class="row"> <div class="col s12"> <label>Language</label> </div> <div each="{languages}" class="col s4"> <label>{name}</label> <select name="_{name}" class="browser-default"> <option each="{list}" value="{name}">{name}</option> </select> </div> </div> </div> </form> </div>', 'id="detailmodal" class="modal bottom-sheet"', function(opts) {
+riot.tag('detailmodal', '<div class="modal-content"> <h4>Setting</h4> <form name="_form" class="row"> <div class="col s6"> <h5>Project</h5> <div class="row"> <div class="col s12 input-field"> <input name="_title" value="hoge" type="text"> <label>Project Title</label> </div> <div class="col s12 input-field"> <textarea name="_description" class="materialize-textarea"></textarea> <label>Description</label> </div> </div> <div class="row"> <div class="col s12"> <label>Language</label> </div> <div each="{languages}" class="col s4"> <label>{name}</label> <select name="_{name}" class="browser-default"> <option each="{list}" value="{name}">{name}</option> </select> </div> </div> </div> </form> </div>', 'detailmodal { max-height: 85% !important; }', 'id="detailmodal" class="modal bottom-sheet"', function(opts) {
     this.languages = [
       {
         name: 'html',
@@ -222,7 +260,7 @@ riot.tag('footer', '', 'footer { position: fixed; height: 30px; width: 100%; bac
 riot.tag('header', '<nav class="blue-grey darken-3"> <div class="nav-wrapper"><a href="#home" class="brand-logo"><img src="/images/runstant.png"><span>Run</span><span class="lighter">stant</span></a> <ul class="right hide-on-small-and-down"> <li data-tooltip="play" class="tooltipped"><a id="btn-play" href="#"><i class="mdi-av-play-arrow"></i></a></li> <li data-tooltip="share" class="tooltipped"><a id="btn-share" href="#"><i class="mdi-social-share"></i></a></li> <li data-tooltip="setting" class="tooltipped"><a id="btn-setting" href="#"><i class="mdi-action-settings"></i></a></li> </ul> <ul id="nav-mobile" style="left: -250px;" class="side-nav"> <li><a href="#">Share</a></li> </ul> <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="mdi-navigation-menu"></i></a> </div> </nav> <style scoped="scoped"> :scope { display: block } nav { padding: 0px 20px; } .brand-logo { position: relative; white-space: nowrap; } .brand-logo img { height: 40px; transform: translate(0px, 5px); } .brand-logo .lighter { font-weight: 200; } </style>', function(opts) {var self = this;
 });
 
-riot.tag('preview', '<div class="inner z-depth-2"> <div class="header cyan lighten-5 grey-text text-darken-2"><span class="title">preview</span></div> <div class="content"> <div id="preview"></div> </div> <btn-fullscreen query="preview"></btn-fullscreen> </div>', 'preview { } preview .inner { background: white; } preview .header { padding: 3px 10px; height: 36px; line-height: 36px; } preview .header .title { font-size: 1.2rem; } #preview { width: 100%; height: 100%; } #preview iframe { width: 100%; height: 100%; border: none; }', function(opts) {
+riot.tag('preview', '<div class="inner z-depth-2"> <div onclick="runstant.openDetailModal();" class="header cyan lighten-5 grey-text text-darken-2"><span class="title">preview</span></div> <div class="content"> <div id="preview"></div> </div> <btn-fullscreen query="preview"></btn-fullscreen> </div>', 'preview { } preview .inner { background: white; } preview .header { padding: 3px 10px; height: 36px; line-height: 36px; } preview .header .title { font-size: 1.2rem; } #preview { width: 100%; height: 100%; } #preview iframe { width: 100%; height: 100%; border: none; }', function(opts) {
     var self = this;
     this.root.style.width = opts.width;
     this.root.style.height = opts.height;
